96b60179ec2000339a34c4d183e1f0846ab5528d
Rohit Prasad <pdrohit674@gmail.com>
2025-04-14 22:21:36 +0530

    fixed floating button size

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -1,15 +1,14 @@
-import {Pressable} from 'react-native';
-import {Plus} from 'phosphor-react-native'; // or your icon lib
-import {View} from 'react-native';
+import { Pressable, View } from 'react-native';
+import { Plus } from 'phosphor-react-native';
 
 const FloatingButton = () => {
     return (
-        <View className="absolute bottom-20 right-6">
+        <View className="absolute bottom-24 right-6">
             <Pressable
-                className="bg-gray-800 w-14 h-14 rounded-full items-center justify-center shadow-lg"
+                className="bg-gray-800 w-16 h-16 rounded-full items-center justify-center shadow-lg"
                 onPress={() => console.log('Add new alarm')}
             >
-                <Plus size={24} color="white" weight="bold" />
+                <Plus size={28} color="white" weight="bold" />
             </Pressable>
         </View>
     );
