5c947d1fb89000f6c571bf84193fbc42d715d202
Rohit Prasad <pdrohit674@gmail.com>
2025-04-20 12:33:47 +0530

    styling corrected in floatingbutton

--- components/FloatingButton.tsx
+++ components/FloatingButton.tsx
@@ -1,12 +1,14 @@
 import { Pressable, View } from 'react-native';
 import { Plus } from 'phosphor-react-native';
+import { useRouter } from 'expo-router';
 
 const FloatingButton = () => {
+    const router = useRouter();
     return (
         <View className="absolute bottom-24 right-6">
             <Pressable
                 className="bg-gray-800 w-16 h-16 rounded-full items-center justify-center shadow-lg"
-                onPress={() => console.log('Add new alarm')}
+                onPress={() => router.push("../../screens/alarms/addAlarm")}
             >
                 <Plus size={28} color="white" weight="bold" />
             </Pressable>
