4d943b893526dcfe0661126578aaace09207b1f6
Rohit Prasad <pdrohit674@gmail.com>
2025-04-20 15:08:26 +0530

    added top navigation in add alarms

--- app/(tabs)/alarms/addAlarm.tsx
+++ app/(tabs)/alarms/addAlarm.tsx
@@ -1,13 +1,29 @@
-import { Stack } from 'expo-router';
-import { View, Text } from 'react-native';
+import {useRouter, Stack} from 'expo-router';
+import {View, Text, ScrollView, Pressable} from 'react-native';
+import PhosphorIcon from "@/components/PhosphorIcon";
+
 
 export default function AddAlarm() {
+
+    const router = useRouter();
     return (
         <>
-            <Stack.Screen options={{ title: 'Add Alarm', headerShown: false }} />
-            <View>
-                <Text>This is the Add Alarm screen</Text>
-            </View>
+            <Stack.Screen options={{title: 'Add Alarm', headerShown: false}}/>
+            <ScrollView style={{flex: 1, gap: 16, padding: 16, backgroundColor: '#EBEBEB'}}>
+                <View className="flex-1 flex-row items-center justify-between">
+                    <View>
+                        <Pressable onPress={() => router.back()} className="">
+                            <PhosphorIcon name={"CaretLeft"} size={24} weight={"bold"}/>
+                        </Pressable>
+                    </View>
+                    <View>
+                        <Text className="text-gray-600 text-lg font-semibold">Rings in: 5h 45m</Text>
+                    </View>
+                    <View>
+                        <PhosphorIcon name={"TrashSimple"} size={24} weight={"transparent"}/>
+                    </View>
+                </View>
+            </ScrollView>
         </>
     );
 }
